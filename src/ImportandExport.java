ab5f9f865e489b49ec199fce6a233328fcbe8101
Acer <yuriel.villanueva@gmail.com>
2024-10-20 02:01:08 +0800

    Fixed, First Project without help with others.

--- src/ImportandExport.java
+++ src/ImportandExport.java
@@ -1,31 +1,29 @@
 import java.util.Scanner;
 
-public class Expressions {
+public class ImportandExport {
     public static void main(String[]yuri){
         Scanner sc = new Scanner(System.in);
-        double num2 = .2;
+        double num2 = 0;
         int num = 0;
-        double ans = 0;
         boolean flag = true;
         while(true){
             System.out.print("Enter Number: ");
             if(sc.hasNextDouble()){
-                num2 += sc.nextDouble();
-                ans++;
+                num += sc.nextDouble();
+                num2++;
             }else {
                 System.out.print("Invalid number...");
-                sc.next();
-                continue;
+                break;
             }
             System.out.print("Do you want to enter another number? (Y/N): ");
             sc.nextLine();
             if(sc.nextLine().equalsIgnoreCase("N")){
-                System.out.print("The average of the entered numbers is: " + num2);
+                double ave = num / num2;
+                System.out.printf("The average of the entered numbers is: " + ave);
                 break;
             }
         }
     }
 }
-
 
 
